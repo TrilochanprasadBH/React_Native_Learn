f2d785918e8fc5da6f555b277085c9daca0fc3b9
TrilochanprasadBH <104064013+TrilochanprasadBH@users.noreply.github.com>
2024-01-02 01:20:58 +0530

    styling basic

--- first_project/project01/App.tsx
+++ first_project/project01/App.tsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { JSXElementConstructor } from "react";
 import {
   View,
   Text,
-  SafeAreaView} from "react-native"
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme
+} from 'react-native';
 
 
 //safe area view is used so that , the edges , notches on phone do not affect rendering on screen 
-function App(){
-  
-  
-return (
-    <SafeAreaView>
-    <View>
-      <Text>
-          This is my first React Native App. 
-      </Text>
-    </View>
-  </SafeAreaView>
+function App() {
+  const toggleColor = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={toggleColor ? styles.whiteText : styles.darkText}>This is my first react native app</Text>
+      </View>
+    </SafeAreaView>
   )
- 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  whiteText: {
+    color: '#FFFFFF'
+  },
+  darkText: {
+    color: '#000000'
+  }
+})
+
 
 export default App
